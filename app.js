162fee5a821dd9fc9a73d748e639cdad03ea0d08
q3640850400 <910626231@qq.com>
2018-04-09 23:30:30 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict"
+
+console.log(`I\`m client`)
+
+const WebSocket =require('ws')
+
+const options={
+    x:1,
+    y:2
+}
+const ws=new WebSocket(
+    'ws://localhost:3005',options
+    
+)
+
+ws.on('open', function open() {
+    console.log(`连接成功`)
+    ws.send('something');
+  });
+  
+ws.on('message', function incoming(data) {
+    console.log(data);
+});
